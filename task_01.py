500c412c9b5131c09ac35219715aea49298649e1
Anatoly S <anatoly@LAPTOP-07LU5AD5.localdomain>
2022-12-02 11:40:33 +0300

    refactor task_01

--- task_01.py
+++ task_01.py
@@ -3,9 +3,7 @@ import re
 
 def is_palindrome(string):
     string = re.sub(r'[\W_]', '', str(string).lower())
-    if bool(string):
-        return string == string[::-1]
-    return False
+    return string == string[::-1] if bool(string) else False
 
 
 print(is_palindrome("A man, a plan, a canal -- Panama"))
@@ -13,3 +11,4 @@ print(is_palindrome("Madam, I'm Adam!"))
 print(is_palindrome(333))
 print(is_palindrome(None))
 print(is_palindrome("Abracadabra"))
+print(is_palindrome("   "))
